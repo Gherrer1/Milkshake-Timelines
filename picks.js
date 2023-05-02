33132b9a28db4c0086094e04137b1573b7fbf132
Gerardo Herrera <gherrer1@binghamton.edu>
2023-05-01 19:10:14 -0700

    round 1 picks

--- picks.js
+++ picks.js
@@ -1,10 +1,15 @@
 /*
   indices for each series:
-  Celtics-Cavs 0
-  Rockets-Warriors 1
-  Warriors-Cavs 2
+  0 Milwaukee vs Miami
+  1 Celtics vs Hawks
+  2 6ers vs Nets
+  3 Cavs vs Knicks
+  4 Nuggets vs Wolves
+  5 Grizz vs Lakers
+  6 Kings vs Warriors
+  7 Suns vs Clippers
 
-  values:
+  Outcome values:
   0 - fave in 4
   1 - fave in 5
   2 - fave in 6
@@ -14,103 +19,219 @@
   6 - dog in 5
   7 - dog in 4
 */
-const leftPad = require('left-pad');
+const leftPad = (str, numSeries, padChar) => str.padStart(numSeries, padChar);
 const picks = {
-  vaughn: {
+  jerry: {
     startPoints: 0,
-    0: 5,
-    1: 5,
+    // picks
+    0: 1,
+    1: 0,
     2: 1,
-    lockitin: 0, // index
+    3: 3,
+    4: 2,
+    5: 2,
+    6: 4,
+    7: 2,
+    lockitin: 1, // index
   },
   fray: {
     startPoints: 0,
-    0: 5,
-    1: 6,
+    0: 0,
+    1: 1,
     2: 1,
-    lockitin: 2,
+    3: 2,
+    4: 1,
+    5: 2,
+    6: 5,
+    7: 1,
+    lockitin: 0,
   },
   gret: {
     startPoints: 0,
-    0: 6,
-    1: 5,
+    0: 1,
+    1: 0,
     2: 1,
-    lockitin: 2,
+    3: 2,
+    4: 1,
+    5: 5,
+    6: 4,
+    7: 2,
+    lockitin: 0,
   },
   flex: {
     startPoints: 0,
-    0: 5,
-    1: 5,
+    0: 1,
+    1: 0,
     2: 1,
-    lockitin: 0,
+    3: 4,
+    4: 1,
+    5: 5,
+    6: 4,
+    7: 2,
+    lockitin: 1,
   },
   mark: {
     startPoints: 0,
-    0: 6,
-    1: 5,
-    2: 1,
+    0: 2,
+    1: 1,
+    2: 0,
+    3: 2,
+    4: 1,
+    5: 3,
+    6: 3,
+    7: 2,
     lockitin: 2,
   },
   kaf: {
     startPoints: 0,
-    0: 6,
+    0: 1,
     1: 2,
-    2: 2,
-    lockitin: 2,
+    2: 1,
+    3: 2,
+    4: 0,
+    5: 3,
+    6: 5,
+    7: 2,
+    lockitin: 3,
   },
   ian: {
     startPoints: 0,
-    0: 5,
-    1: 5,
+    0: 1,
+    1: 0,
     2: 1,
-    lockitin: 2,
+    3: 2,
+    4: 0,
+    5: 1,
+    6: 3,
+    7: 2,
+    lockitin: 1,
   },
   scott: {
     startPoints: 0,
-    0: 3,
-    1: 5,
-    2: 0,
-    lockitin: 2,
+    0: 1,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 1,
+    5: 3,
+    6: 5,
+    7: 1,
+    lockitin: 1,
   },
   can: {
     startPoints: 0,
-    0: 6,
-    1: 5,
-    2: 2,
-    lockitin: 0,
+    0: 0,
+    1: 1,
+    2: 3,
+    3: 4,
+    4: 1,
+    5: 3,
+    6: 5,
+    7: 2,
+    lockitin: 4,
   },
   ryan: {
     startPoints: 0,
-    0: 5,
-    1: 5,
+    0: 1,
+    1: 0,
     2: 1,
+    3: 5,
+    4: 0,
+    5: 2,
+    6: 4,
+    7: 1,
     lockitin: 0,
   },
   will: {
     startPoints: 0,
-    0: 5,
-    1: 6,
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 2,
+    4: 1,
+    5: 2,
+    6: 4,
+    7: 2,
+    lockitin: 0
+  },
+  gratz: {
+    startPoints: 0,
+    0: 1,
+    1: 1,
     2: 1,
-    lockitin: 2
-  }
+    3: 2,
+    4: 1,
+    5: 2,
+    6: 3,
+    7: 2,
+    lockitin: 0
+  },
+  mikemorris: {
+    startPoints: 0,
+    0: 1,
+    1: 2,
+    2: 1,
+    3: 4,
+    4: 3,
+    5: 4,
+    6: 5,
+    7: 1,
+    lockitin: 0
+  },
+  johndefalco: {
+    startPoints: 0,
+    0: 1,
+    1: 2,
+    2: 1,
+    3: 2,
+    4: 0,
+    5: 4,
+    6: 5,
+    7: 2,
+    lockitin: 0
+  },
 }
-const teams = ['BOS', 'CLE', 'HOU', 'GS', 'GS', 'CLE'];
-const possibleOutcomes = [
-  [4, 4],
-  [4, 4],
-  [0, 7],
+const teams = [
+  'MIL', 'MIA',
+  'BOS', 'ATL',
+  'PHI', 'BKN',
+  'CLE', 'NYK',
+  'DEN', 'MIN',
+  'MEM', 'LAL',
+  'SAC', 'GS ',
+  'PHX', 'LAC'
 ];
-const NUM_SERIES = possibleOutcomes.length;
+// Outcome Ranges values (again):
+// 0 - fave in 4
+// 1 - fave in 5
+// 2 - fave in 6
+// 3 - fave in 7
+// 4 - dog in 7
+// 5 - dog in 6
+// 6 - dog in 5
+// 7 - dog in 4
+const outcomesRanges = [
+  [6, 6], // mil vs mia
+  [2, 2], // bos vs atl
+  [0, 0], // phi vs bkn
+  [6, 6], // cle vs nyk
+  [1, 1], // den vs min
+  [3, 5], // mem vs lal
+  [3, 4], // sac vs gs
+  [1, 1], // phx vs lac
+];
+const NUM_SERIES = outcomesRanges.length;
 const POSSIBILITIES = Math.pow(8, NUM_SERIES);
 const lastPlaceStats = {};
 const oneVoneRecord = {};
+const fourAndFiveLosers = [];
 Object.keys(picks).forEach(name => lastPlaceStats[name] = 0);
 resetCurrentRoundScores();
 
 // iterate through every possible timeline
 let numPossibleTimelines = 0;
 let tiesArray = [];
-for(let i = 0; i < 3; i++) {
+for(let i = 0; i < 15; i++) {
   tiesArray.push(0);
 }
 
@@ -126,6 +247,9 @@ for(let i = 0; i < POSSIBILITIES; i++) {
   let lastPlaceFinishers = getLastPlace();
   lastPlaceFinishers.forEach(name => lastPlaceStats[name]++);
   tiesArray[lastPlaceFinishers.length]++;
+  if (lastPlaceFinishers.length >= 4) {
+    fourAndFiveLosers.push(getResultsStrings(outcomes));
+  }
 
   console.log(getResultsStrings(outcomes));
   console.log(lastPlaceFinishers);
@@ -133,10 +257,12 @@ for(let i = 0; i < POSSIBILITIES; i++) {
   resetCurrentRoundScores();
 }
 console.log(`Num timelines left: ${numPossibleTimelines}`);
+console.log('Number of timelines each person finishes last in:');
 console.log(lastPlaceStats);
-for(let i = 1; i < 3; i++) {
+for(let i = 1; i < 16; i++) {
   console.log(`Num ${i} way ties left: ${tiesArray[i]}`);
 }
+console.log(fourAndFiveLosers);
 
 function getSeriesResultsString(seriesNumber, outcome) {
   faveTeamIndex = seriesNumber * 2;
@@ -211,9 +337,9 @@ function updateScoresForTimeline(outcomes) {
 function timelinePossible(timelineOctStr) {
   for(let i = 0; i < timelineOctStr.length; i++) {
     let seriesOutcome = timelineOctStr[i];
-    let possibleOutcomesRange = possibleOutcomes[i];
-    let minPoss = possibleOutcomesRange[0];
-    let maxPoss = possibleOutcomesRange[1];
+    let outcomesRangesRange = outcomesRanges[i];
+    let minPoss = outcomesRangesRange[0];
+    let maxPoss = outcomesRangesRange[1];
     if(seriesOutcome < minPoss || seriesOutcome > maxPoss) {
       return false;
     }
